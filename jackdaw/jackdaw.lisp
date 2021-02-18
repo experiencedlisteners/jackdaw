90956709040169a512b04e524dcf3564ab2dfe74
Bastiaan van der Weij <bjvanderweij@gmail.com>
2021-02-18 20:36:58 +0100

    Rename ngram-accumulator -> ngram

--- jackdaw/jackdaw.lisp
+++ jackdaw/jackdaw.lisp
@@ -162,7 +162,7 @@ stem. For example, if S is :^X, (BASENAME S) is :X."
     (mapcar (lambda (s) (cons s $^self)) ,constraint)
     (mapcar #'list ,(or initialization-constraint constraint))))
 
-(defmacro ngram-accumulator (constraint &optional initialization-constraint n)
+(defmacro ngram (constraint n &optional initialization-constraint)
   `(recursive 
     (mapcar (lambda (s) (cons s (subseq $^self 0 (1- ,n)))) ,constraint)
     (mapcar (lambda (s)	(cons s (loop repeat (1- ,n) collect +ngram-filler+)))
