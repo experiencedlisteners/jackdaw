a42ce71c26090287c6ab70e4ba504341cfb07399
Bastiaan van der Weij <bjvanderweij@gmail.com>
2021-02-20 22:48:33 +0100

    Update tests and add more

--- tests.lisp
+++ tests.lisp
@@ -20,11 +20,6 @@
 ;; With observations (nil t) this should generate a state with symbol t and probability 3/16
 ;; 
 
-(defmodel test-1 () ()
-  ((a () (uniform ()) ())
-   (b (a ^b) (uniform ()) ())
-   (c () (uniform ()) ())))
-
 (defparameter *all-tests* nil)
 
 (defun test-function-symbol (function)
@@ -102,35 +97,72 @@
 		(hash-table->plist hash-table-b)
 		:key-test key-test
 		:value-test value-test))
-    
-(defmacro deftest-with-model (name (model &rest args) &body test)
-  `(deftest ,name
-     (let ((model (make-instance ',model ,@args)))
-       ,@test)))
 
-(deftest-with-model state-variables (test-1 :observe '(c))
-  (assert (equal (jackdaw::state-variables model) '(b))))
 
-(deftest-with-model model-variables (test-1 :observe '(c))
-  (assert (equal (jackdaw::model-variables model) '(b c))))
+;;;;;;;; Unit tests ;;;;;;;;;;;
 
-(deftest-with-model rotate-state (test-1 :observe '(c))
+(defmodel test-1 (dynamic-bayesian-network) ()
+  ((:a () (uniform ())
+       '(:x :y)
+    :key #'first)
+   (:b (:a :^a) (uniform (:a))
+       (list (list :p $a $^a) (list :q $a $^a)))
+   (:c (:b) (uniform ())
+      (list (car $b))
+    :key (lambda (m) (if (listp m) (second m) m)))))
+
+(deftest-with-model congruent-values (test-1)
+  (test (sets-equal (jackdaw::congruent-values
+		     model :a (make-hash-table))
+		    '(:x :y) :test #'equal))
+  (test (sets-equal (jackdaw::congruent-values
+		     model :b (plist->hash-table '(:a :x :^a :y)))
+		    '((:p :x :y) (:q :x :y)) :test #'equal))
+  (test (sets-equal (jackdaw::congruent-values
+		     model :c (plist->hash-table '(:b (:p :x :y))))
+		    '(:p) :test #'equal)))
+
+(deftest-with-model generate (test-1 :observe '(:c))
+  (let ((posterior-congruent-states (generate model '(:x))))
+    (test (equal (length posterior-congruent-states) 2))))
+
+(deftest-with-model state-variables (test-1 :observe '(:c))
+  (test (equal (jackdaw::state-variables model) '(:b))))
+
+(deftest-with-model model-variables (test-1 :observe '(:c))
+  (test (equal (jackdaw::model-variables model) '(:b :c))))
+
+(deftest-with-model rotate-state (test-1 :observe '(:c))
   (let ((state (plist->hash-table
 		'(:probability 1
-		  ^a 0
-		  ^b 1
-		  ^c 0
-		  a 2
-		  b 3
-		  c 4))))
-    (assert (plists-equal (hash-table->plist (jackdaw::rotate-state model state :keep-trace? nil))
+		  :^a 0
+		  :^b 1
+		  :^c 0
+		  :a 2
+		  :b 3
+		  :c 4))))
+    (test (plists-equal (hash-table->plist (jackdaw::rotate-state model state :keep-trace? nil))
 			  '(:probability 1
-			    ^b 3
-			    ^c 4)))))
-					
+			    :^b 3
+			    :^c 4)))))
+
+(defmodel test-2 (dynamic-bayesian-network) ()
+  ((:a () (bernouilli ())
+       '(:x :y)
+    :key #'first)
+   (:b (:a) (cpt (:a))
+       '(:p :q)
+    :key (lambda (m) (if (listp m) (second m) m)))))
+
+(deftest-with-model estimate (test-2 :observe '(:a :b))
+  (jackdaw::estimate model '(((:x :p) (:x :q) (:y :p) (:y :q) (:y :q))))
+  (test (approx-equal (exp (probability (jackdaw::model-variable-distribution model :a) '(:y)))
+		      (/ 3 5)))
+  (test (approx-equal (exp (probability (jackdaw::model-variable-distribution model :b) '(:q :y)))
+		      (/ 2 3))))
 
 ;;(deftest-with-model estimate (test-1)
-;;  (assert (equal (jackdaw::estimate model ) '(b c))))
+;;  (test (equal (jackdaw::estimate model ) '(b c))))
 
 (defun make-state (probability variables values)
   (let ((state (make-hash-table)))
@@ -149,12 +181,12 @@
 	   (make-state (pr:in .3) '(a b) '(0 1))
 	   (make-state (pr:in .4) '(a b) '(1 0))
 	   (make-state (pr:in .2) '(a b) '(1 1)))))
-    (assert (equal (length (jackdaw::marginalize states nil)) 1))
-    (assert (approx-equal (pr:out (gethash :probability (car (jackdaw::marginalize states nil)))) 1))
-    (assert (equal (length (jackdaw::marginalize states '(a))) 2))
-    (assert (equal (gethash 'a (first (jackdaw::marginalize states '(a))))
+    (test (equal (length (jackdaw::marginalize states nil)) 1))
+    (test (approx-equal (pr:out (gethash :probability (car (jackdaw::marginalize states nil)))) 1))
+    (test (equal (length (jackdaw::marginalize states '(a))) 2))
+    (test (equal (gethash 'a (first (jackdaw::marginalize states '(a))))
 		   0) () "Unexpected value of first marginal state. Maybe order changed?")
-    (assert (approx-equal (pr:out (gethash :probability (first (jackdaw::marginalize states '(a)))))
+    (test (approx-equal (pr:out (gethash :probability (first (jackdaw::marginalize states '(a)))))
 			  .4))))
 
 		   
